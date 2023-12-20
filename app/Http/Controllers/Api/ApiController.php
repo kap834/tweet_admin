1b45c94c8d5a012468e30f67ad4533e1283fbaa1
kap834 <aungphyo9900@outlook.com>
2023-12-20 12:20:03 +0630

    add: authentication api

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    public function register(Request $request){
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+        User::create($data);
+        $user = User::where('email', $request->email)->first();
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken(time())->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(isset($user)){
+            if(Hash::check($request->password, $user->password)){
+                return response()->json([
+                    'user' => $user,
+                    'token' => $user->createToken(time())->plainTextToken
+                ], 200);
+            }
+        }else{
+            return response()->json([
+            'user' => 'null',
+            'token' => 'null'
+        ], 200);
+        }
+    }
+}
